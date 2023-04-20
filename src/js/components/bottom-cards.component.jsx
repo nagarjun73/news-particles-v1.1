fb867ec67d2678ab2eacc3b908e6ebea3cc59aa1
nagarjun73 <nagarjun7397@gmail.com>
2023-04-20 17:35:49 +0530

    added links to articles

--- src/js/components/bottom-cards.component.jsx
+++ src/js/components/bottom-cards.component.jsx
@@ -13,15 +13,20 @@ const cardHtml = (arts, i) => {
     
       <div class="card-body d-flex flex-sm-fill">
       <div class="col-md-8 ">
-        <h4 class="card-title">${arts.title.toUpperCase()}</h4>
+        <h4 class="card-title"><a href="${
+          arts.url
+        }" target="_blank">${arts.title.toUpperCase()}</a></h4>
         <p class="card-text">${arts.description}</p>
-        <p class="card-text"><small class="text-muted">${arts.publishedAt.slice(
-          0,
-          10
-        ).split("-").reverse().join("-")}</small></p>
+        <p class="card-text"><small class="text-muted">${arts.publishedAt
+          .slice(0, 10)
+          .split("-")
+          .reverse()
+          .join("-")}</small></p>
         </div>
         <div class="card-img col-md-4 ">
-      <img src="${arts.urlToImage}" class="img-fluid" alt="${arts.source.name}'s article image">
+      <img src="${arts.urlToImage}" class="img-fluid" alt="${
+    arts.source.name
+  }'s article image">
     </div>
     </div> 
   </div>
